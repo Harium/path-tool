b3133f30a23949bbe89086c31f1cc9cb5a759118
Yuri Pourre <yuripourre@gmail.com>
2022-01-16 19:45:02 -0800

    Handle optimized paths

--- src/main/java/com/harium/etyl/geometry/path/svg/importer/SVGPathParser.java
+++ src/main/java/com/harium/etyl/geometry/path/svg/importer/SVGPathParser.java
@@ -17,7 +17,8 @@ public class SVGPathParser {
 
     private static final String CLOSE = "z";
     private static final String CLOSE_UPPERCASE = "Z";
-    private static final String MOVE = "M";
+    private static final String MOVE_RELATIVE = "m";
+    private static final String MOVE_ABSOLUTE = "M";
     private static final String LINE_ABSOLUTE = "L";
     private static final String LINE_RELATIVE = "l";
     private static final String HORIZONTAL_LINE_ABSOLUTE = "H";
@@ -39,51 +40,64 @@ public class SVGPathParser {
 
         Path2D path = new Path2D();
         Point2D lastPoint = new Point2D();
+
+        String lastCommand = MOVE_ABSOLUTE;
+
         for (int i = 0; i < parts.length; i++) {
-            if (MOVE.equals(parts[i])) {
-                lastPoint = parseMove(parts, lastPoint, i);
+            String currentPart = parts[i];
+            if (!Character.isLetter(currentPart.charAt(0))) {
+                currentPart = lastCommand;
+                i--;
+            }
+
+            if (MOVE_ABSOLUTE.equals(currentPart)) {
+                lastPoint = parseMoveAbsolute(parts, lastPoint, i);
                 i+=2;
-            } else if (LINE_ABSOLUTE.equals(parts[i])) {
+            } else if (MOVE_RELATIVE.equals(currentPart)) {
+                lastPoint = parseMoveRelative(parts, lastPoint, i);
+                i+=2;
+            } else if (LINE_ABSOLUTE.equals(currentPart)) {
                 lastPoint = parseLineAbsolute(parts, path, lastPoint, i);
                 i+=2;
-            } else if (LINE_RELATIVE.equals(parts[i])) {
+            } else if (LINE_RELATIVE.equals(currentPart)) {
                 lastPoint = parseLineRelative(parts, path, lastPoint, i);
                 i+=2;
-            } else if (HORIZONTAL_LINE_ABSOLUTE.equals(parts[i])) {
+            } else if (HORIZONTAL_LINE_ABSOLUTE.equals(currentPart)) {
                 lastPoint = parseHorizontalLineAbsolute(parts, path, lastPoint, i);
                 i+=1;
-            } else if (HORIZONTAL_LINE_RELATIVE.equals(parts[i])) {
+            } else if (HORIZONTAL_LINE_RELATIVE.equals(currentPart)) {
                 lastPoint = parseHorizontalLineRelative(parts, path, lastPoint, i);
                 i+=1;
-            } else if (VERTICAL_LINE_ABSOLUTE.equals(parts[i])) {
+            } else if (VERTICAL_LINE_ABSOLUTE.equals(currentPart)) {
                 lastPoint = parseVerticalLineAbsolute(parts, path, lastPoint, i);
                 i+=1;
-            } else if (VERTICAL_LINE_RELATIVE.equals(parts[i])) {
+            } else if (VERTICAL_LINE_RELATIVE.equals(currentPart)) {
                 lastPoint = parseVerticalLineRelative(parts, path, lastPoint, i);
                 i+=1;
-            } else if (QUADRATIC_ABSOLUTE.equals(parts[i])) {
+            } else if (QUADRATIC_ABSOLUTE.equals(currentPart)) {
                 lastPoint = parseQuadraticAbsolute(parts, path, lastPoint, i);
                 i+=4;
-            } else if (QUADRATIC_RELATIVE.equals(parts[i])) {
+            } else if (QUADRATIC_RELATIVE.equals(currentPart)) {
                 lastPoint = parseQuadraticRelative(parts, path, lastPoint, i);
                 i+=4;
-            } else if (CUBIC_ABSOLUTE.equals(parts[i])) {
+            } else if (CUBIC_ABSOLUTE.equals(currentPart)) {
                 lastPoint = parseCubicAbsolute(parts, path, lastPoint, i);
                 i+=6;
-            } else if (CUBIC_RELATIVE.equals(parts[i])) {
+            } else if (CUBIC_RELATIVE.equals(currentPart)) {
                 lastPoint = parseCubicRelative(parts, path, lastPoint, i);
                 i+=6;
-            } else if (SHORTHAND_CUBIC_ABSOLUTE.equals(parts[i])) {
+            } else if (SHORTHAND_CUBIC_ABSOLUTE.equals(currentPart)) {
                 lastPoint = parseShorthandCubicAbsolute(parts, path, lastPoint, i);
                 i+=4;
-            } else if (SHORTHAND_CUBIC_RELATIVE.equals(parts[i])) {
+            } else if (SHORTHAND_CUBIC_RELATIVE.equals(currentPart)) {
                 lastPoint = parseShorthandCubicRelative(parts, path, lastPoint, i);
                 i+=4;
-            } else if (CLOSE.equals(parts[i]) || CLOSE_UPPERCASE.equals(parts[i])) {
+            } else if (CLOSE.equals(currentPart) || CLOSE_UPPERCASE.equals(currentPart)) {
                 lastPoint = parseClose(parts, path, lastPoint, i);
                 paths.add(path);
                 path = new Path2D();
             }
+            lastCommand = currentPart;
         }
         if (!path.isEmpty()) {
             paths.add(path);
@@ -92,7 +106,8 @@ public class SVGPathParser {
     }
 
     String cleanData(String d) {
-        return d.replaceAll(MOVE, " " + MOVE + " ")
+        return d.replaceAll(MOVE_ABSOLUTE, " " + MOVE_ABSOLUTE + " ")
+                .replaceAll(MOVE_RELATIVE, " " + MOVE_RELATIVE + " ")
                 .replaceAll(LINE_ABSOLUTE, " " + LINE_ABSOLUTE + " ")
                 .replaceAll(LINE_RELATIVE, " " + LINE_RELATIVE + " ")
                 .replaceAll(HORIZONTAL_LINE_ABSOLUTE, " " + HORIZONTAL_LINE_ABSOLUTE + " ")
@@ -113,11 +128,19 @@ public class SVGPathParser {
                 .trim();
     }
 
-    private Point2D parseMove(String[] parts, Point2D lastPoint, int i) {
+    private Point2D parseMoveAbsolute(String[] parts, Point2D lastPoint, int i) {
         double x = Double.parseDouble(parts[i + 1]);
         double y = Double.parseDouble(parts[i + 2]);
 
         lastPoint = new Point2D(x, y);
+        return lastPoint;
+    }
+
+    private Point2D parseMoveRelative(String[] parts, Point2D lastPoint, int i) {
+        double x = Double.parseDouble(parts[i + 1]);
+        double y = Double.parseDouble(parts[i + 2]);
+
+        lastPoint = new Point2D(lastPoint.x + x, lastPoint.y + y);
         return lastPoint;
     }
 
--- src/test/java/com/harium/etyl/geometry/path/svg/importer/SVGPathParserTest.java
+++ src/test/java/com/harium/etyl/geometry/path/svg/importer/SVGPathParserTest.java
@@ -1,7 +1,10 @@
 package com.harium.etyl.geometry.path.svg.importer;
 
+import com.harium.etyl.geometry.Path2D;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -20,5 +23,13 @@ public class SVGPathParserTest {
         assertEquals("3 4", parser.cleanData("3,  4"));
         assertEquals("1 2 3 4 5 6", parser.cleanData("1 2,  3 4 , 5 6"));
         assertEquals("1 2 3 4 5 6", parser.cleanData(" 1 2  ,  3 4 ,5  6 "));
+    }
+
+    @Test
+    public void testOptimizedPath() {
+        String pathData = "m 66.245779,98.660037 c -0.670162,17.928703 13.800311,40.768673 35.734251,40.234503 21.93394,-0.53417 36.10017,-23.9415 36.10017,-40.269479 0,-16.327976 -8.55014,-41.14763 -35.37301,-40.743461 -26.82286,0.404169 -35.791249,22.849737 -36.461411,40.778437 z";
+        List<Path2D> result = parser.parseData(pathData);
+
+        assertEquals(4, result.get(0).getCurves().size());
     }
 }
